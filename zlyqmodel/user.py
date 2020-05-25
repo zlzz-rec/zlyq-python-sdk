fdff6f543c5ef69cb7419c0383a6fb084480b9fb
jiangsiyiJ41 <jiangsiyij41@gmail.com>
2020-05-25 12:18:30 +0800

    userFollow

--- zlyqmodel/user.py
+++ zlyqmodel/user.py
@@ -1,30 +1,32 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class UserInfo:
-    id:            str = ""
     thirdId:       str = ""
     account:       str = ""
-    nickname:       str = ""
-    avatar:       str = ""
-    gender:       int = 0
-    birthday:       int = 0
-    signature:       str = ""
-    region:       str = ""
-    extraInfo:       str = ""
-    createdAt:       int = 0
-    updatedAt:       int = 0
-    loginTime:       int = 0
+    nickname:      str = ""
+    avatar:        str = ""
+    gender:        int = 0
+    birthday:      int = 0
+    signature:     str = ""
+    region:        str = ""
+    extraInfo:     str = ""
+    createdAt:     int = 0
+    updatedAt:     int = 0
+    loginTime:     int = 0
     isRobot:       int = 0
-    status:       int = 0
-    avatarStorage:       bool = False
-    phone:       str = ""
-    deviceId:       str = ""
-    deviceType:       str = ""
+    status:        int = 0
+    avatarStorage: bool = False
+    phone:         str = ""
+    deviceId:      str = ""
+    deviceType:    str = ""
 
 
 @dataclass
 class UserFollow:
     userId:       str = ""
-    followMap:    dict = {} # key:str value:int
+    followMap: dict = field(default_factory=dict) # key:str value:int
+
+    def add(self, k, v):
+        self.followMap[k] = v
--- zlyqtest/usertest.py
+++ zlyqtest/usertest.py
@@ -1,21 +1,27 @@
 from ..zlyqsync.client import SyncClient
-from ..zlyqmodel.user import UserInfo
+from ..zlyqmodel.user import UserInfo, UserFollow
 
 if __name__ == "__main__":
     userClient = SyncClient(
-            appKey = "29d9c1ec0eeaaa4eed534d676633a909", 
-            appSecret = "c3bf54192a63f047030f0bfb83df6055", 
+            appKey = "29d9c1ec0eeaaa4eed534d676633a909",
+            appSecret = "c3bf54192a63f047030f0bfb83df6055",
             appId = 457639568360448000,
             address = "http://testappapi.zplatform.cn"
     )
-    
+
     userInfo = UserInfo()
-    userInfo.thirdId = '51982'
-    userInfo.udid = "ABC"
-    userInfo.nickname = "test name"
-    userInfo.gender = 1
-    userInfo.account = "xiaoming"
-    userInfo.avatar = "http://www.baidu.com"
-    userInfo.phone = "10000000000"
+    userInfo.thirdId = "51982"
+    # userInfo.udid = "ABC"
+    # userInfo.nickname = "test name"
+    # userInfo.gender = 1
+    # userInfo.account = "xiaoming"
+    # userInfo.avatar = "http://www.baidu.com"
+    # userInfo.phone = "10000000000"
 
     print(userClient.userInfoSynchronize(userInfo))
+
+    # userFollow = UserFollow()
+    # userInfo.userId = '51982'
+    # userFollow.add('51983', 0)
+    #
+    # print(userClient.userFollowSynchronize(userFollow))
