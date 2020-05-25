b3e50ca70204288b3ac6da1f9b04ae9b6f873a8e
jiangsiyiJ41 <jiangsiyij41@gmail.com>
2020-05-25 11:18:42 +0800

    userFollow

--- zlyqmodel/user.py
+++ zlyqmodel/user.py
@@ -1,23 +1,45 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class UserInfo:
-    thirdId      :       str   = ""
-    account      :       str   = ""
-    nickname     :       str   = ""
-    avatar       :       str   = ""
-    gender       :       int   = 0
-    birthday     :       int   = 0
-    signature    :       str   = ""
-    region       :       str   = ""
-    extraInfo    :       str   = ""
-    createdAt    :       int   = 0
-    updatedAt    :       int   = 0
-    loginTime    :       int   = 0
-    isRobot      :       int   = 0
-    status       :       int   = 0
-    avatarStorage:       bool  = False
-    phone        :       str   = ""
-    deviceId     :       str   = ""
-    deviceType   :       str   = ""
+    thirdId:       str = ""
+    account:       str = ""
+    nickname:       str = ""
+    avatar:       str = ""
+    gender:       int = 0
+    birthday:       int = 0
+    signature:       str = ""
+    region:       str = ""
+    extraInfo:       str = ""
+    createdAt:       int = 0
+    updatedAt:       int = 0
+    loginTime:       int = 0
+    isRobot:       int = 0
+    status:       int = 0
+    avatarStorage:       bool = False
+    phone:       str = ""
+    deviceId:       str = ""
+    deviceType:       str = ""
 
+
+@dataclass
+class UserFollow:
+    thirdId:       str = ""
+    account:       str = ""
+    nickname:       str = ""
+    avatar:       str = ""
+    gender:       int = 0
+    birthday:       int = 0
+    signature:       str = ""
+    region:       str = ""
+    extraInfo:       str = ""
+    createdAt:       int = 0
+    updatedAt:       int = 0
+    loginTime:       int = 0
+    isRobot:       int = 0
+    status:       int = 0
+    avatarStorage:       bool = False
+    phone:       str = ""
+    deviceId:       str = ""
+    deviceType:       str = ""
