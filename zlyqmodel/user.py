c41f42611bb482f6ac84e02522807752a8bf0045
jiangsiyiJ41 <jiangsiyij41@gmail.com>
2020-05-25 11:38:24 +0800

    userFollow

--- zlyqmodel/user.py
+++ zlyqmodel/user.py
@@ -3,6 +3,7 @@ from dataclasses import dataclass
 
 @dataclass
 class UserInfo:
+    id:            str = ""
     thirdId:       str = ""
     account:       str = ""
     nickname:       str = ""
@@ -25,21 +26,5 @@ class UserInfo:
 
 @dataclass
 class UserFollow:
-    thirdId:       str = ""
-    account:       str = ""
-    nickname:       str = ""
-    avatar:       str = ""
-    gender:       int = 0
-    birthday:       int = 0
-    signature:       str = ""
-    region:       str = ""
-    extraInfo:       str = ""
-    createdAt:       int = 0
-    updatedAt:       int = 0
-    loginTime:       int = 0
-    isRobot:       int = 0
-    status:       int = 0
-    avatarStorage:       bool = False
-    phone:       str = ""
-    deviceId:       str = ""
-    deviceType:       str = ""
+    userId:       str = ""
+    followMap:    dict = {} # key:str value:int
