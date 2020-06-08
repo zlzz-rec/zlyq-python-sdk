2de7efe966520b0af98750a5466fa78bd498889b
Clifford <290416118@qq.com>
2020-06-08 14:29:47 +0800

    rm

--- zlyqauth/private_sign.py
+++ zlyqauth/private_sign.py
@@ -1,24 +1,6 @@
 import time
 import hashlib
 
-def addSign(params, apiKey):
-    params['time'] = int(time.time() * 1000)
-
-    keys = [key for key in params.keys()]
-    keys.sort()
-
-    values = []
-    for key in keys:
-        values.append(key + '=' + str(params[key]))
-
-    string = "&".join(values)
-
-    salt = apiKey
-    signature = hashlib.md5((salt + "&" + string).encode(encoding='UTF-8')).hexdigest()
-
-    return signature, string
-
-
 @dataclass
 class Signer():
     validTime:int = 5 * 60 * 1000
